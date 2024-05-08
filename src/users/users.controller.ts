b8c9c8e8c7cd2c388cd7fe43f7d4d2a9d98c16e5
madou9 <issoufou.mamadou@yahoo.com>
2024-05-08 15:52:23 +0200

    First commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    /*
+        GET request /Users,
+        GET request /Users/:id,
+        POST /Users
+        PATH /Users/:id
+        DELETE /Users/:id
+    */
+}
